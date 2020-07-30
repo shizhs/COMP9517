df9cdc48db4ed28c9e012b4fa50355d76702f3d2
sakethgetla <saketh.getla@gmail.com>
2020-07-30 23:07:03 +1000

    autopushed

--- create_imgs.py
+++ create_imgs.py
@@ -9,8 +9,10 @@ import matplotlib.pyplot as plt
 import math
 from scipy import ndimage as ndi
 from skimage.feature import peak_local_max
+import imageio
 
 
+##
 ##
 def con_stretch(img):
     c = 255
@@ -59,31 +61,45 @@ def fast_min_max_filter(img, N, max_neighbour):
     return out
 ##
 #imgp1 = cv2.imread("./Image_Sequences/PhC-C2DL-PSC/Sequence 1/t001.tif")
-path = "./Image_Sequences/PhC-C2DL-PSC/Sequence 1/t001.tif"
-image = Image.open(path).convert('L')
-image = color.rgb2gray(np.asarray(image))
+path = "./Image_Sequences/PhC-C2DL-PSC/Sequence 1/t098.tif"
+mask_path = "./Image_Sequences/PhC-C2DL-PSC/Sequence 1 Masks/t098mask.tif"
+
+#image = Image.open(path).convert('L')
+image = np.array(Image.open(path))
+
+whole_img = Image.open(mask_path)
+#whole_img = np.array(Image.open(mask_path))
+mask =  Image.open(mask_path)
+mask = np.array(mask)
+#whole_img.show()
+whole_img = np.array(whole_img)
 
 ##
-#iteration = 3
-#for i in range(iteration):
-#    img = ndi.minimum_filter(image.copy(), size=3)
-#for i in range(iteration):
-#    img = ndi.maximum_filter(img, size=3)
-
-
-#filter_size = 3
-#img = image.copy()
-#img = ndi.minimum_filter(img, size=filter_size)
-#img = ndi.maximum_filter(img, size=filter_size)
-
-#image = image - img
-img = background_subtract(image.copy(), 3, 0)
 fig=plt.figure()
 fig.add_subplot(2, 2, 1)
 plt.imshow(image)
+
+img = background_subtract(image.copy(), 4, 0)
 fig.add_subplot(2, 2, 2)
 plt.imshow(img)
+
+im2 = Image.open(path)
+im2 = np.array(im2)
+im2 = ndi.minimum_filter(im2, size=9)
+im2 = ndi.maximum_filter(im2, size=9)
+O = image.copy() - im2
+con_stretch(O)
+fig.add_subplot(2, 2, 3)
+plt.imshow(O)
+
+fig.add_subplot(2, 2, 4)
+con_stretch(mask)
+plt.imshow(mask)
+
 plt.show()
+##
+
+contours, hierarchy = cv2.findContours(O.copy().astype(np.uint8), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
 
 ##
 #img = Image.open(path).convert('L')
@@ -96,7 +112,9 @@ im2.show()
 img.show()
 ##
 
+contours, hierarchy = cv2.findContours(img.copy().astype(np.uint8), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
 
+##
 imgplot = plt.imshow(img)
 plt.show()
 #img = Image.open(path).convert("LA")
@@ -140,49 +158,49 @@ img = ndi.maximum_filter(image, size=10, mode='constant')
 
 ##
 #image = image[:,:,0]
-i =1
-image = img
-image = cv2.fastNlMeansDenoising(image,50,50,7,21)
-sigma=15/(2*math.sqrt(2*np.log(2)))
-image=cv2.GaussianBlur(image,(3, 3),sigma, sigma)
-image = ndi.maximum_filter(image, size=10, mode='constant')
-local_max = peak_local_max(image, indices=False, min_distance=15)
-fig=plt.figure()
-fig.add_subplot(1, 4, 1)
-plt.imshow(img, cmap="gray")
-local_max=local_max.astype(np.uint8)
-fig.add_subplot(1, 4, 2)
-plt.title("local maxima")
-plt.imshow(local_max, cmap="gray")
-
-fig.add_subplot(1, 4, 3)
-ret,otsu = cv2.threshold(image,0,255, cv2.THRESH_OTSU)
-plt.title("Otsu on max filtered image")
-plt.imshow(otsu, cmap="gray")
-
-fig.add_subplot(1, 4, 4)
-m = ndi.maximum_filter(otsu, size=5, mode='constant')
-#     m = ndi.minimum_filter(m, size=10, mode='constant')
-plt.imshow(m, cmap="gray")
-image = cv2.bitwise_and(otsu,otsu,mask = local_max)
-#     fig.add_subplot(3, 4, i*4+4)
-plt.title("Otsu and local maxima")
-plt.imshow(image, cmap="gray")
-plt.show()
-#return m
-##
-fig=plt.figure()
-fig.add_subplot(1, 2, 1)
-plt.imshow(img, cmap="gray")
+#i =1
+#image = img
+#image = cv2.fastNlMeansDenoising(image,50,50,7,21)
+#sigma=15/(2*math.sqrt(2*np.log(2)))
+#image=cv2.GaussianBlur(image,(3, 3),sigma, sigma)
+#image = ndi.maximum_filter(image, size=10, mode='constant')
+#local_max = peak_local_max(image, indices=False, min_distance=15)
+#fig=plt.figure()
+#fig.add_subplot(1, 4, 1)
+#plt.imshow(img, cmap="gray")
+#local_max=local_max.astype(np.uint8)
+#fig.add_subplot(1, 4, 2)
+#plt.title("local maxima")
+#plt.imshow(local_max, cmap="gray")
+#
+#fig.add_subplot(1, 4, 3)
+#ret,otsu = cv2.threshold(image,0,255, cv2.THRESH_OTSU)
+#plt.title("Otsu on max filtered image")
+#plt.imshow(otsu, cmap="gray")
+#
+#fig.add_subplot(1, 4, 4)
+#m = ndi.maximum_filter(otsu, size=5, mode='constant')
+##     m = ndi.minimum_filter(m, size=10, mode='constant')
+#plt.imshow(m, cmap="gray")
+#image = cv2.bitwise_and(otsu,otsu,mask = local_max)
+##     fig.add_subplot(3, 4, i*4+4)
+#plt.title("Otsu and local maxima")
+#plt.imshow(image, cmap="gray")
 #plt.show()
-fig.add_subplot(1, 2, 2)
-plt.imshow(image, cmap="gray")
-plt.show()
-##
-
-#contours, hierarchy = cv2.findContours(img.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-contours, hierarchy = cv2.findContours(img.copy().astype(np.uint8), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
-print(contours)
-print(hierarchy)
+##return m
+###
+#fig=plt.figure()
+#fig.add_subplot(1, 2, 1)
+#plt.imshow(img, cmap="gray")
+##plt.show()
+#fig.add_subplot(1, 2, 2)
+#plt.imshow(image, cmap="gray")
+#plt.show()
+###
+#
+##contours, hierarchy = cv2.findContours(img.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+#contours, hierarchy = cv2.findContours(img.copy().astype(np.uint8), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
+#print(contours)
+#print(hierarchy)
 ##
 
