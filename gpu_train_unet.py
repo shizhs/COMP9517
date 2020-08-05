104ed3ade6cd2718dee6aa8a2ce9f78ba4943918
sakethgetla <saketh.getla@gmail.com>
2020-08-05 10:40:22 +1000

    autopushed

---
+++ gpu_train_unet.py
@@ -0,0 +1,203 @@
+import torch
+import Augmentor
+from PIL import Image, ImageFilter
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from gpu_unet import UNET
+from torch import nn
+from train import train_net
+from torchvision import transforms, utils, datasets
+import torchvision
+import matplotlib.image as mpimg
+import torch.nn.functional as F
+from pdb import set_trace as bp
+
+def normalize(v): 
+    norm = np.linalg.norm(v) 
+    if norm == 0: 
+        return v 
+    return v / norm
+
+def find_traning_data(mask_path, img_path):
+    imgs = []
+    masks = []
+    for i in range(100):
+        try:
+            if i < 10 :
+                masks.append(mpimg.imread(mask_path +'t00'+ str(i) + 'mask.tif').astype(np.float64))
+                imgs.append(mpimg.imread(img_path +'t00'+ str(i) + '.tif').astype(np.float64))
+            else:
+                masks.append(mpimg.imread(mask_path +'t0'+ str(i) + 'mask.tif').astype(np.float64))
+                imgs.append(mpimg.imread(img_path +'t0'+ str(i) + '.tif').astype(np.float64))
+
+            #imgs.append(normalize( mpimg.imread(img_path +'t0'+ str(i) + '.tif').astype(np.float64) ))
+            #fig=plt.figure(figsize=(10, 10))
+            #fig.add_subplot(2, 1, 1)
+            #plt.imshow(imgs[-1])
+            #fig.add_subplot(2, 1, 2)
+            #plt.imshow(masks[-1])
+            #plt.show()
+        except:
+            pass
+    return imgs, masks
+
+##
+img_path = './Image_Sequences/DIC-C2DH-HeLa/Sequence 1/'
+mask_path = './Image_Sequences/DIC-C2DH-HeLa/Sequence 1 Masks/'
+imgs, masks = find_traning_data(mask_path, img_path)
+
+img_path2 = './Image_Sequences/DIC-C2DH-HeLa/Sequence 2/'
+mask_path2 = './Image_Sequences/DIC-C2DH-HeLa/Sequence 2 Masks/'
+imgs2, masks2 = find_traning_data(mask_path2, img_path2)
+imgs = imgs + imgs2
+masks = masks2 + masks
+#imgs.append(imgs2)
+#masks.append(masks2)
+#imgs = normalize(imgs)
+
+##
+test_imgs = []
+for i in range(84):
+    if i < 10 :
+        test_imgs.append(torch.tensor(mpimg.imread(img_path +'t00'+ str(i) + '.tif').astype(np.float64)).unsqueeze(0))
+        #test_imgs.append(mpimg.imread(img_path +'t00'+ str(i) + '.tif').astype(np.float64))
+    else:
+        test_imgs.append(torch.tensor(mpimg.imread(img_path +'t0'+ str(i) + '.tif').astype(np.float64)).unsqueeze(0))
+        #test_imgs.append(mpimg.imread(img_path +'t0'+ str(i) + '.tif').astype(np.float64))
+##
+
+#image = mpimg.imread(path).astype(np.float64)
+#mask = Image.open(mask_path)
+#mask = mpimg.imread(mask_path).astype(np.float64)
+
+class DogDataset3(Dataset):
+    '''
+    Sample dataset for Augmentor demonstration.
+    The dataset will consist of just one sample image.
+    '''
+    def __init__(self, image, mask):
+        self.image = image
+        self.mask = mask
+
+    def __len__(self):
+        return 1 # return 1 as we have only one image
+
+    def __getitem__(self, idx):
+        # Returns the augmented image
+        
+        # Initialize the pipeline
+        p = Augmentor.DataPipeline([[np.array(self.image), np.array(self.mask)]])
+
+        # Apply augmentations
+        p.zoom_random(0.8, percentage_area=0.7) # zoom randomly with 50% probability
+        p.rotate(0.8, max_left_rotation=10, max_right_rotation=10) # rotate the image with 50% probability
+        p.shear(0.5, max_shear_left = 10, max_shear_right = 10) # shear the image with 50% probability
+        p.flip_random(0.5)
+        #p.random_brightness(0.5, 0.9, 1.1)
+
+        # Sample from augmentation pipeline
+        images_aug = p.sample(idx)
+        
+        # Get augmented image
+        #augmented_image = torch.tensor(images_aug)
+        #
+        # convert to tensor and return the result
+        #bp()
+        #return augmented_image.unsqueeze(2)
+        return images_aug
+
+
+##
+
+device = torch.device('cuda')
+def train(model, imgs, masks, optimizer, epochs): 
+    print('start training')
+    #dataset = []
+    #counter =0
+    #for img, mask in zip(imgs, masks):
+    #    print('change img ', counter)
+    #    counter += 1
+    #    train_ds = DogDataset3(img, mask)
+    #    #dataset.append(train_ds[50])
+    #    dataset += train_ds[50]
+
+    #dataset = torch.tensor(dataset).unsqueeze(2)
+    #bp()
+    #train_dataset = torch.utils.data.TensorDataset(dataset[:, 0], dataset[:, 1])
+    #trainloader = DataLoader(train_dataset, batch_size=5, shuffle=True)
+
+    for i in range(epochs):
+        batch = 0
+        print('epoch ', i)
+        for data, target in trainloader:
+            optimizer.zero_grad()    # zero the gradients
+            data, target = data.to(device), target.to(device)
+            output = model(data)       # apply network
+            #loss = nn.CrossEntropyLoss(output, target)
+            loss = F.mse_loss(output, target)
+            #loss = F.cross_entropy(torch.flatten(output, end_dim=-2), torch.flatten(target, end_dim=-2))
+            #loss = F.nll_loss(torch.flatten(output, end_dim=-2), torch.flatten(target, end_dim=-2))
+            batch += 1
+            print('batch %int training loss: ', batch, loss)
+            loss.backward()          # compute gradients
+            optimizer.step()         # update weights
+
+def test(model, data):
+    model.eval()
+    with torch.no_grad():
+        for x in data:
+            output = model(x.float().unsqueeze(0))
+            fig=plt.figure(figsize=(10, 10))
+            fig.add_subplot(2, 1, 1)
+            plt.imshow(x[0])
+            fig.add_subplot(2, 1, 2)
+            plt.imshow(output[0][0])
+            plt.show()
+
+##
+dataset = []
+counter =0
+for img, mask in zip(imgs, masks):
+    print('change img ', counter)
+    counter += 1
+    train_ds = DogDataset3(img, mask)
+    #dataset.append(train_ds[50])
+    dataset += train_ds[20]
+
+dataset = torch.tensor(dataset).unsqueeze(2)
+#bp()
+train_dataset = torch.utils.data.TensorDataset(dataset[:, 0], dataset[:, 1])
+trainloader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+
+##
+model = UNET(1, 1).to(device)
+#optimizer = torch.optim.Adam(model.parameters(),eps=0.000001,lr=0.0001, betas=(0.9,0.999),weight_decay=0.0001)
+#optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, betas=(0.9,0.999))
+
+##
+train(model, imgs, masks, optimizer, 1)
+
+##
+#test_imgs = normalize(test_imgs)
+#test(model, torch.tensor(test_imgs[:3]).unsqueeze(1))
+#test(model, torch.tensor(test_imgs[:3]))
+test(model, test_imgs[15:19])
+#test(model, torch.tensor(imgs[:3]).unsqueeze(1))
+##
+
+torch.save(model, './models/M4')
+
+
+##
+
+
+#train_ds = DogDataset3(image, mask)
+#
+## Initialize the dataloader
+#data_set = train_ds[13]
+#train_dataset = torch.utils.data.TensorDataset(data_set[:, 0],data_set[:, 1])
+#trainloader = DataLoader(train_dataset, batch_size=3, shuffle=False)
+##bp()
---
+++ gpu_unet.py
@@ -0,0 +1,142 @@
+
+#https://medium.com/analytics-vidhya/creating-a-very-simple-u-net-model-with-pytorch-for-semantic-segmentation-of-satellite-images-223aa216e705
+
+from torch import nn
+import torch
+import time
+
+class UNET(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.conv1 = self.contract_block(in_channels, 32, 7, 3)
+        self.conv2 = self.contract_block(32, 64, 3, 1)
+        self.conv3 = self.contract_block(64, 128, 3, 1)
+
+        self.upconv3 = self.expand_block(128, 64, 3, 1)
+        self.upconv2 = self.expand_block(64*2, 32, 3, 1)
+        self.upconv1 = self.expand_block(32*2, out_channels, 3, 1)
+
+    #def forward(self, x):
+    def __call__(self, x):
+
+        # downsampling part
+        conv1 = self.conv1(x)
+        conv2 = self.conv2(conv1)
+        conv3 = self.conv3(conv2)
+
+        upconv3 = self.upconv3(conv3)
+
+        upconv2 = self.upconv2(torch.cat([upconv3, conv2], 1))
+        upconv1 = self.upconv1(torch.cat([upconv2, conv1], 1))
+
+        return upconv1
+
+    def contract_block(self, in_channels, out_channels, kernel_size, padding):
+
+        contract = nn.Sequential(
+            torch.nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=1, padding=padding),
+            torch.nn.BatchNorm2d(out_channels),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=1, padding=padding),
+            torch.nn.BatchNorm2d(out_channels),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+                                 )
+
+        return contract
+
+    def expand_block(self, in_channels, out_channels, kernel_size, padding):
+
+        expand = nn.Sequential(
+            torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=padding),
+            torch.nn.BatchNorm2d(out_channels),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(out_channels, out_channels, kernel_size, stride=1, padding=padding),
+            torch.nn.BatchNorm2d(out_channels),
+            torch.nn.ReLU(),
+            torch.nn.ConvTranspose2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1)
+        )
+        return expand
+
+def train(model, train_dl, valid_dl, loss_fn, optimizer, acc_fn, epochs=1):
+    start = time.time()
+    model.cuda()
+    #model.cpu()
+
+    train_loss, valid_loss = [], []
+
+    best_acc = 0.0
+
+    for epoch in range(epochs):
+        print('Epoch {}/{}'.format(epoch, epochs - 1))
+        print('-' * 10)
+
+        #for phase in ['train', 'valid']:
+        for phase in ['train']:
+            if phase == 'train':
+                model.train(True)  # Set trainind mode = true
+                dataloader = train_dl
+            else:
+                model.train(False)  # Set model to evaluate mode
+                dataloader = valid_dl
+
+            running_loss = 0.0
+            running_acc = 0.0
+
+            step = 0
+
+            # iterate over data
+            #for x, y in dataloader:
+            #for x, y in train_dl:
+            #for a in train_dl:
+            for x, y in train_dl:
+                #x = x.cuda()
+                #y = y.cuda()
+                x = a.to('gpu')
+                y = a.to('gpu')
+                step += 1
+
+                # forward pass
+                if phase == 'train':
+                    # zero the gradients
+                    optimizer.zero_grad()
+                    outputs = model(x[:])
+                    loss = loss_fn(outputs, y[:])
+
+                    # the backward pass frees the graph memory, so there is no 
+                    # need for torch.no_grad in this training pass
+                    loss.backward()
+                    optimizer.step()
+                    # scheduler.step()
+
+                else:
+                    with torch.no_grad():
+                        outputs = model(x)
+                        loss = loss_fn(outputs, y.long())
+
+                # stats - whatever is the phase
+                acc = acc_fn(outputs, y)
+
+                running_acc  += acc*dataloader.batch_size
+                running_loss += loss*dataloader.batch_size 
+
+                if step % 10 == 0:
+                    # clear_output(wait=True)
+                    print('Current step: {}  Loss: {}  Acc: {}  AllocMem (Mb): {}'.format(step, loss, acc, 0))
+                    # print(torch.cuda.memory_summary())
+
+            epoch_loss = running_loss / len(dataloader.dataset)
+            epoch_acc = running_acc / len(dataloader.dataset)
+
+            print('{} Loss: {:.4f} Acc: {}'.format(phase, epoch_loss, epoch_acc))
+
+            train_loss.append(epoch_loss) if phase=='train' else valid_loss.append(epoch_loss)
+
+    time_elapsed = time.time() - start
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))    
+    
+    return train_loss, valid_loss    
+
+def acc_metric(predb, yb):
+    return (predb.argmax(dim=1) == yb.cuda()).float().mean()
